46c03a44b244941df3f9832afbf3de2a89af3f33
Diego Andrés <diegoandres_cortes@outlook.com>
2021-10-02 09:13:43 -0500

    russian translation
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ Themes/default/languages/SimpleReferrals/.russian.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @package Simple Referrals
+ * @version 1.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2021, SMF Tricks
+ * @license https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
+
+$txt['SimpleReferrals_referred'] = 'Реферер';
+$txt['SimpleReferrals_referrer_desc'] = 'Пользователь, который привел вас на форум.';
+$txt['SimpleReferrals_count_total'] = 'Всего рефералов';
+$txt['SimpleReferrals_link'] = 'Реферальная ссылка';
+
+$txt['maintain_referrals'] = 'Рефералы';
+$txt['maintain_recountreferrals'] = 'Пересчёт рефералов пользователей';
+$txt['maintain_recountreferrals_info'] = 'Запустите эту задачу обслуживания, чтобы обновить количество рефералов пользователей. Он пересчитает всех (учитываемых) рефералов, а затем обновит итоговые показатели рефералов в профилях.';
+$txt['SimpleReferrals_settings'] = 'Настройки Simple Referrals';
+$txt['SimpleReferrals_allow_select'] = 'Разрешить только прямые ссылки';
+$txt['SimpleReferrals_allow_select_desc'] = 'Если включено, гости не смогут находить/устанавливать пользователей форума в форме регистрации, смогут использовать только прямые реферальные ссылки.';
+$txt['SimpleReferrals_enable_posts'] = 'Отображать количество рефералов в сообщениях';
+$txt['SimpleReferrals_enable_profile'] = 'Отображать количество рефералов в профиле';
+$txt['SimpleReferrals_display_link'] = 'Отображать реферальную ссылку в профиле';
