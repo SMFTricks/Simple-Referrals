cd93380c7bd55abf804d3dea28d159e478c4e1cc
Diego Andrés <diegoandres_cortes@outlook.com>
2021-10-16 08:39:27 -0500

    add portuguese
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ Themes/default/languages/SimpleReferrals/.portuguese_pt.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @package Simple Referrals
+ * @version 1.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2021, SMF Tricks
+ * @license https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
+
+$txt['SimpleReferrals_referred'] = 'Recomendado por';
+$txt['SimpleReferrals_referrer_desc'] = 'O membro que te recomendou o fórum.';
+$txt['SimpleReferrals_count_total'] = 'Total Recomendações';
+$txt['SimpleReferrals_link'] = 'Link para Recomendações';
+
+$txt['maintain_referrals'] = 'Recomendações';
+$txt['maintain_recountreferrals'] = 'Recontar as Recomendações do Utilizador';
+$txt['maintain_recountreferrals_info'] = 'Execute esta tarefa de manutenção para atualizar a contagem de recomendações de utilizadores. Ela recontará todas as recomendações (contáveis) que eles têm e, em seguida, atualizará os totais de recomendações nos perfis deles';
+$txt['SimpleReferrals_settings'] = 'Configurações de Recomendações Simples';
+$txt['SimpleReferrals_allow_select'] = 'Permitir apenas links diretos';
+$txt['SimpleReferrals_allow_select_desc'] = 'Se ativado, os convidados não poderão pesquisar/definir utilizadores do fórum no formulário de registo, só podem usar links de recomendação diretos.';
+$txt['SimpleReferrals_enable_posts'] = 'Permitir a contagem de recomendações em postagens';
+$txt['SimpleReferrals_enable_profile'] = 'Permitir a contagem de recomendações no perfil';
+$txt['SimpleReferrals_display_link'] = 'Exibir o link para recomendação no perfil';
+
+// Stats
+$txt['SimpleReferrals_enable_stats'] = 'Exibir os principais recomendadores nas estatísticas do fórum';
+$txt['top_most_referrals'] = 'Membros com mais recomendações';
+
+// Alerts
+$txt['SimpleReferrals_send_alerts'] = 'Enviar alerta para os recomendadores sobre novos registos';
+$txt['alert_group_referrals'] = $txt['maintain_referrals'];
+$txt['alert_new_referred'] = 'Quando alguém se regista usando o meu link de recomendação';
+$txt['alert_referral_new_referred'] = '{member_link} acabou de usar o seu link de recomendação';
